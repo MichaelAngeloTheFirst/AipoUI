1fee7733b7076603e6d8bb187b8c2a3e89cad602
Dawid <DawidSroka123@interia.pl>
2024-06-17 14:32:23 +0200

    add best gui

--- aipo_ui.py
+++ aipo_ui.py
@@ -1,69 +1,77 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
-from PyQt5.QtGui import QIcon
-
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QFileDialog, QHBoxLayout
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+import requests
 
 class App(QWidget):
     def __init__(self):
         super().__init__()
-        self.title = "PyQt5 file dialogs - pythonspot.com"
+        self.title = "AiPO"
         self.left = 10
         self.top = 10
-        self.width = 640
-        self.height = 480
+        self.width = 420
+        self.height = 380
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        self.openFileNameDialog()
-        self.openFileNamesDialog()
-        self.saveFileDialog()
+        layout = QVBoxLayout()
+
+        self.label = QLabel('No image selected')
+        self.label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.label)
+
+        buttonLayout = QHBoxLayout()
+
+        self.selectButton = QPushButton('Select image')
+        self.selectButton.clicked.connect(self.openFileNameDialog)
+        self.selectButton.setStyleSheet("background-color: lightblue;")
+        buttonLayout.addWidget(self.selectButton)
+
+        self.uploadButton = QPushButton('Check personal information')
+        self.uploadButton.clicked.connect(self.upload_image)
+        self.uploadButton.setStyleSheet("background-color: lightgreen;")
+        buttonLayout.addWidget(self.uploadButton)
+
+        layout.addLayout(buttonLayout)
+
+        self.resultLabel = QLabel('Your personal information')
+        self.resultLabel.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.resultLabel)
+
+        self.setLayout(layout)
 
         self.show()
 
     def openFileNameDialog(self):
         options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(
-            self,
-            "QFileDialog.getOpenFileName()",
-            "",
-            "All Files (*);;Python Files (*.py)",
-            options=options,
-        )
+        options |= QFileDialog.ReadOnly
+        fileName, _ = QFileDialog.getOpenFileName(self, "Select Image", "", "Images (*.png *.xpm *.jpg *.jpeg *.bmp *.gif)", options=options)
         if fileName:
-            print(fileName)
+            self.label.setPixmap(QPixmap(fileName).scaled(400, 400, Qt.KeepAspectRatio))
+            self.fileName = fileName
 
-    def openFileNamesDialog(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        files, _ = QFileDialog.getOpenFileNames(
-            self,
-            "QFileDialog.getOpenFileNames()",
-            "",
-            "All Files (*);;Python Files (*.py)",
-            options=options,
-        )
-        if files:
-            print(files)
+    def upload_image(self):
+        if hasattr(self, 'fileName'):
+            self.resultLabel.setText(self.fileName)
+            # url = 'backend'
+            # files = {'file': open(self.fileName, 'rb')}
+            # response = requests.post(url, files=files)
+            # if response.status_code == 200:
+            #     self.resultLabel.setText(response.text)
+            # else:
+            #     self.resultLabel.setText('Error uploading image')
+        else:
+            self.resultLabel.setText('No Image Selected')
 
-    def saveFileDialog(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getSaveFileName(
-            self,
-            "QFileDialog.getSaveFileName()",
-            "",
-            "All Files (*);;Text Files (*.txt)",
-            options=options,
-        )
-        if fileName:
-            print(fileName)
-
-
-if __name__ == "__main__":
+def main():
     app = QApplication(sys.argv)
     ex = App()
+    ex.show()
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
